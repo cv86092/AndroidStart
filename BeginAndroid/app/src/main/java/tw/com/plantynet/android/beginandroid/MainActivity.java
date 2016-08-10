87bca78dc707b9da3ede67522417474fcfa671a0
cv86092 <cv86092@gmail.com>
2016-08-10 16:56:57 +0800

    20160808 Change

--- BeginAndroid/app/src/main/java/tw/com/plantynet/android/beginandroid/MainActivity.java
+++ BeginAndroid/app/src/main/java/tw/com/plantynet/android/beginandroid/MainActivity.java
@@ -2,12 +2,56 @@ package tw.com.plantynet.android.beginandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+    EditText editText;
+    //Button button;
+
+    TextView textView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView =(TextView)findViewById(R.id.textView);
+        editText= (EditText) findViewById(R.id.editText);
+
+        editText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) {
+                    editText.setText(editText.getText().toString()+Mi"TEST");
+                    submit(v);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+
+
+
     }
+
+
+
+    public void submit(View view){
+        String result = editText.getText().toString();
+        textView.setText(result);
+        editText.setText("");
+    }
+
+
 }

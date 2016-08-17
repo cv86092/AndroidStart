651dbc078a777c7086c293e408f67d7e909a3d10
cv86092 <cv86092@gmail.com>
2016-08-17 10:19:58 +0800

    add bmi

--- BeginAndroid/app/src/main/java/tw/com/plantynet/android/beginandroid/MainActivity.java
+++ BeginAndroid/app/src/main/java/tw/com/plantynet/android/beginandroid/MainActivity.java
@@ -4,8 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 
@@ -14,11 +15,11 @@ public class MainActivity extends AppCompatActivity {
 
 
     EditText editText;
-    //Button button;
-
     TextView textView;
-
-
+    RadioGroup radioGroup;
+    String regex = "男生";
+    EditText editText2;
+    EditText editText3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +27,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView =(TextView)findViewById(R.id.textView);
-        editText= (EditText) findViewById(R.id.editText);
+        editText= (EditText)findViewById(R.id.editText);
+        radioGroup = (RadioGroup)findViewById(R.id.radioGroup);
+        editText2 = (EditText)findViewById(R.id.editText2);
+        editText3 = (EditText)findViewById(R.id.editText3);
+
+
+
 
         editText.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
                 if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) {
-                    editText.setText(editText.getText().toString()+Mi"TEST");
+                    editText.setText(editText.getText().toString());
                     submit(v);
                     return true;
                 }
@@ -40,16 +47,42 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radioButton = (RadioButton) group.findViewById(checkedId);
+                regex = "你是"+radioButton.getText().toString();
+            }
+        });
 
 
 
-    }
+
+        }
+
+
+
+
+//    radioGroup.setOnCheckedChangeListener(rdgSexchkChListener);
+//    public RadioGroup.OnCheckedChangeListener rdgSexchkChListener =
+//            RadioGroup.OnCheckedChangeListener(RadioGroup group, int checkedId)
+//
+//    {
+//        int intChkRb = radioGroup.getCheckedRadioButtonId();
+//    }
+
 
 
 
     public void submit(View view){
         String result = editText.getText().toString();
-        textView.setText(result);
+        int weight = Integer.parseInt(editText2.getText().toString());
+        int height = Integer.parseInt(editText3.getText().toString());
+
+//        int bmi = (weight)/((height/100)*(height/100));
+        double bmi = 50/(1.44*1.44);
+        String bmiValue = String.valueOf(bmi);
+        textView.setText(result + bmiValue);
         editText.setText("");
     }
 
